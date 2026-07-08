ea3fe89a45705b4e14ea5757f5d1e5ead6c22c87
afvanie <ardyansah201@gmail.com>
2026-07-08 11:50:25 +0700

    Fix mobile navbar script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,13 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+    duration: 900,
+    once: true,
+    offset: 100,
+    easing: 'ease-out-cubic',
+});
+
 document.addEventListener("DOMContentLoaded", () => {
 
     console.log("JS NAVBAR AKTIF");
@@ -8,40 +18,47 @@ document.addEventListener("DOMContentLoaded", () => {
     const closeMenu = document.getElementById("closeMenu");
 
     // SCROLL EFFECT
-    window.addEventListener("scroll", () => {
+    if (navbar) {
+        window.addEventListener("scroll", () => {
 
-        if (window.scrollY > 50) {
-            navbar.classList.add("bg-white", "shadow", "text-black");
-            navbar.classList.remove("text-white");
-        } else {
-            navbar.classList.remove("bg-white", "shadow", "text-black");
-            navbar.classList.add("text-white");
-        }
+            if (window.scrollY > 50) {
+                navbar.classList.add("bg-white", "shadow", "text-black");
+                navbar.classList.remove("text-white");
+            } else {
+                navbar.classList.remove("bg-white", "shadow", "text-black");
+                navbar.classList.add("text-white");
+            }
 
-    });
-
-    // OPEN MENU
-    if (mobileButton && mobileMenu) {
-        mobileButton.addEventListener("click", () => {
-            mobileMenu.style.right = "0";
         });
     }
 
-    // CLOSE MENU
+    // OPEN MOBILE MENU
+    if (mobileButton && mobileMenu) {
+        mobileButton.addEventListener("click", () => {
+            console.log("HAMBURGER DIKLIK");
+
+            mobileMenu.style.right = "0";
+        });
+    } else {
+        console.log("mobileButton atau mobileMenu tidak ditemukan");
+    }
+
+    // CLOSE MOBILE MENU
     if (closeMenu && mobileMenu) {
         closeMenu.addEventListener("click", () => {
             mobileMenu.style.right = "-100%";
         });
     }
 
-});
+    // CLOSE MENU AFTER CLICK LINK
+    if (mobileMenu) {
+        const mobileLinks = document.querySelectorAll("#mobileMenu a");
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+        mobileLinks.forEach((link) => {
+            link.addEventListener("click", () => {
+                mobileMenu.style.right = "-100%";
+            });
+        });
+    }
 
-AOS.init({
-    duration: 900,
-    once: true,
-    offset: 100,
-    easing: 'ease-out-cubic',
 });
